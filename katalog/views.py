2079493220d132be527af1cf71dc4f05748feb6d
Kaloosh Falito Verrell <kf.verrell@gmail.com>
2022-09-13 10:55:17 +0700

    Local Commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog_item = CatalogItem.objects.all()
+    context = {
+        'list_item': data_katalog_item,
+        'name': 'Kaloosh Falito Verrell',
+        'npm': '2106720916'
+    }
+    return render(request, "katalog.html", context)
