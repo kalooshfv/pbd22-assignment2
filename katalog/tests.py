95aca9681a9c7ede26223887d8858824ede080e2
Kaloosh Falito Verrell <kf.verrell@gmail.com>
2022-09-15 10:52:12 +0700

    Test

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class KatalogTest(TestCase):
+    def test_url(self):
+        resp = self.client.get('/katalog/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_template(self):
+        resp = self.client.get('/katalog/')
+        self.assertTemplateUsed(resp, 'katalog.html')
