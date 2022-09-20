966252daab8e590e82b47053c6330c5a79a3ef76
unknown <kf.verrell@gmail.com>
2022-09-20 22:10:31 +0700

    Edited tests

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -10,3 +10,11 @@ class KatalogTest(TestCase):
     def test_template(self):
         resp = self.client.get('/mywatchlist/')
         self.assertTemplateUsed(resp, 'mywatchlist.html')
+
+    def test_xml(self):
+        resp = self.client.get('/mywatchlist/xml/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_json(self):
+        resp = self.client.get('/mywatchlist/json/')
+        self.assertEqual(resp.status_code, 200)
