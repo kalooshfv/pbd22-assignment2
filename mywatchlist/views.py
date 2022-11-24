d1f3efd9a5aeaa2658820f2d606c18728559ab04
Kaloosh F. Verrell <kf.verrell@gmail.com>
2022-11-24 07:11:16 +0700

    Added jsonresp

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -3,7 +3,7 @@ from mywatchlist.models import WatchlistItem
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
-import time
+from django.contrib.auth import authenticate, login as auth_login
 
 # Create your views here.
 def show_watchlist(request):
@@ -38,15 +38,19 @@ def show_json(request):
     return HttpResponse(serializers.serialize("json", data), \
         content_type="application/json")
 
+@csrf_exempt
+def show_json_resp(request):
+    data = WatchlistItem.objects.all()
+    return JsonResponse(serializers.serialize("json", data), \
+        content_type="application/json")
+
 def show_xml_by_id(request, id):
     data = WatchlistItem.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), \
         content_type="application/xml")
 
-@csrf_exempt
 def show_json_by_id(request, id):
     data = WatchlistItem.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), \
         content_type="application/json")
-
 
